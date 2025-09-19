e2fe9e324b35e4342cf4ffff1e50e756bfc1aea3
Nicholas Adamou <nicholasadamouemail@gmail.com>
2025-09-19 14:10:59 -0400

    chore: update configsync to v1.0.2
    - Update version to 1.0.2
    - Update URL to point to v1.0.2 release
    - Update SHA256 checksum for new universal binary with proper version injection
    - This release fixes version display issues

--- Formula/configsync.rb
+++ Formula/configsync.rb
@@ -1,10 +1,10 @@
 class Configsync < Formula
   desc "Synchronize macOS application configurations across machines"
   homepage "https://github.com/dotbrains/configsync"
-  url "https://github.com/dotbrains/configsync/releases/download/v1.0.1/configsync-v1.0.1-darwin-universal.tar.gz"
-  sha256 "f32ccad4935cfdac7d74e0d41184d552c8105c93c88086c2d6d56e015d35b209"
+  url "https://github.com/dotbrains/configsync/releases/download/v1.0.2/configsync-v1.0.2-darwin-universal.tar.gz"
+  sha256 "c3a5a56ee88bb4d7045a26f568325e17d76869bee21f2c1616bd5c4ee8616840"
   license "MIT"
-  version "1.0.1"
+  version "1.0.2"
 
   depends_on :macos
 
