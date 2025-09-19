52b7691aeafdece320042f9949b00b8dc3daf322
Nicholas Adamou <nicholasadamouemail@gmail.com>
2025-09-19 14:24:56 -0400

    Update configsync to v1.0.3

--- Formula/configsync.rb
+++ Formula/configsync.rb
@@ -1,10 +1,10 @@
 class Configsync < Formula
   desc "Synchronize macOS application configurations across machines"
   homepage "https://github.com/dotbrains/configsync"
-  url "https://github.com/dotbrains/configsync/releases/download/v1.0.2/configsync-v1.0.2-darwin-universal.tar.gz"
-  sha256 "c3a5a56ee88bb4d7045a26f568325e17d76869bee21f2c1616bd5c4ee8616840"
+  url "https://github.com/dotbrains/configsync/releases/download/v1.0.3/configsync-v1.0.3-darwin-universal.tar.gz"
+  sha256 "50e9a88189a784111d15bca1e032784355798ab60d8ae7621f994cb3cb2cf850"
   license "MIT"
-  version "1.0.2"
+  version "1.0.3"
 
   depends_on :macos
 
